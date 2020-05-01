a7ede552fd0d2ac514a4b8f372037111848f57b7
Jonathan Bayless <baylessj@purdue.edu>
2020-05-01 19:18:37 -0400

    Updated the install file

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
-#! /bin/bash
-sudo apt install vim gnome-tweak
+#! /usr/bin/env zsh
+
+# apt packages to install
+sudo apt install -y gimp git nodejs python3-pip sphinx-common vim virtualbox zip zsh
 
 # install brave
 sudo apt install apt-transport-https curl
@@ -14,4 +16,19 @@ sudo apt install brave-browser
 
 # vscode
 curl -o vscode.deb -L "https://go.microsoft.com/fwlink/?LinkID=760868"
-sudo dpkg -i vscode.deb 
+sudo dpkg -i vscode.deb
+
+# Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# spaceship theme
+CUSTOM_DIR="$HOME/.oh-my-zsh/custom"
+mkdir -p "$CUSTOM_DIR/themes"
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$CUSTOM_DIR/themes/spaceship-prompt"
+ln -s "$CUSTOM_DIR/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/themes/spaceship.zsh-theme"
+
+# Set zsh as default shell
+chsh -s $(which zsh)
+
+# move .rc files
+cp ./zsh/.zshrc $HOME/.zshrc
+cp ./vim/.vimrc $HOME/.vimrc
