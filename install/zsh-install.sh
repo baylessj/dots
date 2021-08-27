7b69df7167d0cae6b2c4bd1c45b80a8267470d86
Jonathan Bayless <jonathanbayless@curative.com>
2021-08-27 10:11:38 -0400

    added some things for curative mac setup

--- install/zsh-install.sh
+++ install/zsh-install.sh
@@ -4,6 +4,6 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 CUSTOM_DIR="$HOME/.oh-my-zsh/custom"
 mkdir -p "$CUSTOM_DIR/themes"
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$CUSTOM_DIR/themes/spaceship-prompt"
-ln -s "$CUSTOM_DIR/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/themes/spaceship.zsh-theme"
+ln -s $HOME/.oh-my-zsh/themes/spaceship-prompt/spaceship.zsh-theme $HOME/.oh-my-zsh/themes/spaceship.zsh-theme
 
 
